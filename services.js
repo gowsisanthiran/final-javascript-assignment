e5c8f20628c4ce722b79c74739e167641187e38b
gowsisanthiran <gowshisanthiran@gmail.com>
2023-02-19 15:22:25 +0530

    modify

--- services.js
+++ services.js
@@ -1,2 +1,13 @@
 window.location.href = "file:///home/gowsi/Downloads/uki-2/uki-2/Assignment/service.html"; 
 alert("404 Error - Page Not Found");
+
+swal("Here's a message!")
+
+
+            // Swal.fire({
+            //     title: "<i>Title</i>", 
+            //     html: "Testno  sporocilo za objekt: <b>test</b>",  
+            //     confirmButtonText: "V <u>redu</u>", 
+            //   });
+
+            //   <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
